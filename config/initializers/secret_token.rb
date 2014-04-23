25d1f43884be271d1b1edfaf2a05d67b277a03ab
Allan Tokuda <praetis@gmail.com>
2014-04-22 20:55:50 -0500

    Blank rails app without ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MetaApp::Application.config.secret_key_base = '785dd18120cd5b0f4cdfdbf18503639699553b4d738a257a69e8809702e936ccfdd380c0ba63cf7568ab00729375254ce48eebebb861c1af70d272636291e439'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MetaApp::Application.config.session_store :cookie_store, key: '_meta_app_session'
