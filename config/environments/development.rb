f98ec7809536ee9e67f673a845453d90bd6167b0
Allan Tokuda <praetis@gmail.com>
2014-04-27 10:28:47 -0500

    No stack trace in response

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,8 +9,10 @@ MetaApp::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  # Do not show error reports in response
+  config.consider_all_requests_local = false
+
+  # Disable caching
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
